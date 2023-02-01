df3ea53c5eeeb387a1dab8d55b88bf972f327798
akulij <48361256+akulij@users.noreply.github.com>
2023-02-01 21:16:51 +0500

    create tests

--- tests.c
+++ tests.c
@@ -1,2 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+#include "drivetype.h"
+
 int main(int argc, char* argv[]) {
+    assert(detect_drive_type("sda") == 0 || detect_drive_type("sda") == 1);
+    assert(detect_drive_type("doesnotexists") == 2);
 }
