ac98a9ae25b0095100201e59f5ccdd9fb5c97749
akulij <48361256+akulij@users.noreply.github.com>
2023-02-01 21:17:11 +0500

    create drive detector

--- drivetype.h
+++ drivetype.h
@@ -1,5 +1,24 @@
 #ifndef DRIVETYPE_H
 #define DRIVETYPE_H
 
+#include <stdio.h>
+
+typedef enum {
+    HDD_DRIVE,
+    SDD_DRIVE,
+    ERR_ACCESS,
+} DriveType;
+
+DriveType detect_drive_type(char* drive_letter) {
+    char drive_type_path[1024];
+    DriveType drive_type;
+
+    sprintf(drive_type_path, "/sys/block/%s/queue/rotational", drive_letter);
+    FILE* f = fopen(drive_type_path, "r");
+    if (!f) return ERR_ACCESS;
+    fscanf(f, "%d", &drive_type);
+
+    return drive_type;
+}
 
 #endif
