350524988660fb875762f14e3de5b903482ee807
akulij <48361256+akulij@users.noreply.github.com>
2023-02-01 21:43:27 +0500

    Colorize test information

--- tests.c
+++ tests.c
@@ -2,7 +2,11 @@
 #include <assert.h>
 #include "drivetype.h"
 
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
 int main(int argc, char* argv[]) {
     assert(detect_drive_type("sda") == 0 || detect_drive_type("sda") == 1);
     assert(detect_drive_type("doesnotexists") == 2);
+    printf(ANSI_COLOR_CYAN "Tests passed!" ANSI_COLOR_RESET "\n");
 }
